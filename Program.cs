7d1312e20a6faed4e954917ada7db7ebd1ac5014
Syed-Raheem <syedabdulraheem801@gmail.com>
2022-07-04 12:47:33 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyApplication;
+using MudBlazor.Services;
+using Blazor.Bluetooth;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+//builder.Services.AddBlazmBluetooth();
+builder.Services.AddBluetoothNavigator();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
